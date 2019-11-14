5db5998a7db5821364b3d8ac00e3c7d8c0e7208d
guzzilar <iam.nuttanon@gmail.com>
2019-11-14 10:30:56 +0700

    Konbini: implementing a foundation code.

---
+++ includes/gateway/class-omise-payment-konbini.php
@@ -0,0 +1,82 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No direct script access allowed.' );
+
+function register_omise_konbini() {
+	require_once dirname( __FILE__ ) . '/class-omise-payment.php';
+
+	/**
+	 * @since 3.10
+	 */
+	class Omise_Payment_Konbini extends Omise_Payment {
+		public function __construct() {
+			parent::__construct();
+
+			$this->id                 = 'omise_konbini';
+			$this->has_fields         = false;
+			$this->method_title       = __( 'Convenience Store / Pay-easy / Online Banking', 'omise' );
+			$this->method_description = wp_kses(
+				__( 'Accept payments through <strong>Convenience Store</strong> / <strong>Pay-easy</strong> / <strong>Online Banking</strong> via Omise payment gateway.', 'omise' ),
+				array( 'strong' => array() )
+			);
+
+			$this->init_form_fields();
+			$this->init_settings();
+
+			$this->title                = $this->get_option( 'title' );
+			$this->description          = $this->get_option( 'description' );
+			$this->restricted_countries = array( 'JP' );
+		}
+
+		/**
+ 		 * @see WC_Settings_API::init_form_fields()
+ 		 * @see woocommerce/includes/abstracts/abstract-wc-settings-api.php
+ 		 */
+		public function init_form_fields() {
+			$this->form_fields = array(
+				'enabled' => array(
+					'title'   => __( 'Enable/Disable', 'omise' ),
+					'type'    => 'checkbox',
+					'label'   => __( 'Enable Omise Convenience Store / Pay-easy / Online Banking Payment', 'omise' ),
+					'default' => 'no'
+				),
+
+				'title' => array(
+					'title'       => __( 'Title', 'omise' ),
+					'type'        => 'text',
+					'description' => __( 'This controls the title the user sees during checkout.', 'omise' ),
+					'default'     => __( 'Convenience Store / Pay-easy / Online Banking', 'omise' ),
+				),
+
+				'description' => array(
+					'title'       => __( 'Description', 'omise' ),
+					'type'        => 'textarea',
+					'description' => __( 'This controls the description the user sees during checkout.', 'omise' )
+				),
+			);
+		}
+
+		/**
+ 		 * @inheritdoc
+ 		 */
+		public function charge( $order_id, $order ) { }
+
+		/**
+		 * @inheritdoc
+		 */
+		public function result( $order_id, $order, $charge ) { }
+	}
+
+	if ( ! function_exists( 'add_omise_konbini' ) ) {
+		/**
+		 * @param  array $methods
+		 *
+		 * @return array
+		 */
+		function add_omise_konbini( $methods ) {
+			$methods[] = 'Omise_Payment_Konbini';
+			return $methods;
+		}
+
+		add_filter( 'woocommerce_payment_gateways', 'add_omise_konbini' );
+	}
+}
--- omise-woocommerce.php
+++ omise-woocommerce.php
@@ -81,6 +81,7 @@ class Omise {
 		register_omise_creditcard();
 		register_omise_installment();
 		register_omise_internetbanking();
+		register_omise_konbini();
 		register_omise_truemoney();
 		prepare_omise_myaccount_panel();
 	}
@@ -131,6 +132,7 @@ class Omise {
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/gateway/class-omise-payment-creditcard.php';
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/gateway/class-omise-payment-installment.php';
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/gateway/class-omise-payment-internetbanking.php';
+		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/gateway/class-omise-payment-konbini.php';
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/gateway/class-omise-payment-truemoney.php';
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/libraries/omise-php/lib/Omise.php';
 		require_once OMISE_WOOCOMMERCE_PLUGIN_PATH . '/includes/libraries/omise-plugin/Omise.php';
